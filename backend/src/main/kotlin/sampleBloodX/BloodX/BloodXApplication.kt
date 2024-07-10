11b46b93936f1621d8d0d2c77c87086a711f4d6a
Bala suja <https://github.com/KaditInnovations-Sandbox>
2024-07-10 17:24:58 +0530

    Initial commit

---
+++ backend/src/main/kotlin/sampleBloodX/BloodX/BloodXApplication.kt
@@ -0,0 +1,11 @@
+package sampleBloodX.BloodX
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BloodXApplication
+
+fun main(args: Array<String>) {
+	runApplication<BloodXApplication>(*args)
+}
---
+++ backend/src/test/kotlin/sampleBloodX/BloodX/BloodXApplicationTests.kt
@@ -0,0 +1,13 @@
+package sampleBloodX.BloodX
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BloodXApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
