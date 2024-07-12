61002f489db96a89b80c23d9edcb82beedc66546
Bala suja <https://github.com/KaditInnovations-Sandbox>
2024-07-12 15:52:44 +0530

    changes updated

--- backend/src/main/kotlin/com/poc/bloodx/model/BloodRequest.kt
+++ backend/src/main/kotlin/com/poc/bloodx/model/BloodRequest.kt
@@ -1,6 +1,7 @@
 package com.poc.bloodx.model
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
 class BloodRequest(var patientname: String,
@@ -8,6 +9,10 @@ class BloodRequest(var patientname: String,
             var age: Int,
             var units: Int,
             var reason: String,
+            var gender: String,
+            var phone: Long,
+            var location: String,
+            var requiredbefore: LocalDate,
             @Id
             @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "admin_seq")
             @SequenceGenerator(name = "admin_seq", sequenceName = "admin_seq", allocationSize = 1)
